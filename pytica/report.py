974a65f578721c3d822fcec13766828be741009c
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-31 09:25:18 +0700

    Update report.py

--- pytica/report.py
+++ pytica/report.py
@@ -4,20 +4,29 @@ from matplotlib.backends.backend_pdf import PdfPages
 from pytica.visualizer import Visualizer
 
 class Report:
-    def __init__(self, grades_file):
-        self.data = pd.read_csv(grades_file)
-        self.visualizer = Visualizer(grades_file)
+    def __init__(self, filename=None, data=None):
+        if filename:
+            self.data = pd.read_csv(filename)
+            self.visualizer = Visualizer(filename=filename)
+        elif data is not None:
+            self.data = data.copy()
+            self.visualizer = Visualizer(data=data)
+        else:
+            self.data = pd.DataFrame(columns=["Name"])
+            self.visualizer = Visualizer(data=self.data)
 
     def generate_excel(self, output_file="report.xlsx"):
-        self.data["Average"] = self.data.iloc[:, 1:].mean(axis=1)
+        if self.data.shape[1] > 1:
+            self.data["Average"] = self.data.iloc[:, 1:].mean(axis=1)
         self.data.to_excel(output_file, index=False)
         print(f"Excel report saved to {output_file}")
 
     def generate_pdf(self, output_file="report.pdf"):
         with PdfPages(output_file) as pdf:
-            self.visualizer.plot_class_average()
-            pdf.savefig()
-            plt.close()
+            if self.data.shape[1] > 1:
+                self.visualizer.plot_class_average()
+                pdf.savefig()
+                plt.close()
             for student in self.data["Name"]:
                 self.visualizer.plot_student_progress(student)
                 pdf.savefig()
